64024f3305c3c6bec30b25c6e23355274d31470e
seongryeonglee1991 <119789313+seongryeonglee1991@users.noreply.github.com>
2024-01-11 09:10:14 +0900

    Update program.py

--- program.py
+++ program.py
@@ -5,7 +5,7 @@ import html
 import os
 from datetime import datetime
 
-# github라는 오픈소스 공간에서 SLACK_WEBHOOK_URL을 암호화하기 위한 코드.
+# github라는 오픈소스 공간에서 SLACK_WEBHOOK_URL을 암호화하기 위한 코드
 SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
 
 def makePayloadItem(newsItem):
