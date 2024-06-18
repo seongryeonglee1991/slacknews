d8ebda163b14d051b26fc0fdb64b3b9d790bccba
seongryeonglee1991 <119789313+seongryeonglee1991@users.noreply.github.com>
2024-06-18 17:26:28 +0900

    Update program.py

--- program.py
+++ program.py
@@ -5,101 +5,95 @@ import html
 import os
 from datetime import datetime
 
-# github라는 오픈소스 공간에서 SLACK_WEBHOOK_URL을 암호화하기 위한 코드
+# GitHub Secrets에서 환경 변수 가져오기
 SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
 
 def makePayloadItem(newsItem):
-  기사링크 = f"<{newsItem['link']}>"
-  pubDate_datetime = datetime.strptime(newsItem['pubDate'], "%a, %d %b %Y %H:%M:%S %Z")
-  pubDate_formatted = pubDate_datetime.strftime("%Y년 %m월 %d일 %H:%M")
-  return      {
-          "type": "section",
-          "color": "#ff0044",
-          "title": f"{html.unescape(newsItem['title'])}",
-          "fields": [
-          {
-           "type": "mrkdwn",
-           "value": f"*{pubDate_formatted}*",
-           "short": True
-          },
-          {
-                    "type": "divider"
-              },
-          {
-               "type": "mrkdwn",
-               "value": f"{기사링크}",
-               "short": 'true'
-              },
-          {
-                    "type": "divider"
-              }
-          ]
-      }
-
-def callWebhook (payload):
-  headers = {
-    'Content-type' : 'application/json',
-  }
-  res = requests.post(SLACK_WEBHOOK_URL, headers=headers, json=payload)
-  print(res.text)
-
-def getNewsFromRss():
-    try:
-        RSS_URL = 'https://news.google.com/rss/search?hl=ko&gl=KR&ceid=KR%3Ako&oc=11&q=%22%ED%85%8C%EC%9D%B4%EB%B8%94%EC%98%A4%EB%8D%94%22%20when%3A1d'
-        res = requests.get(RSS_URL)
-        ordered_dict = xmltodict.parse(res.text)
-        json_type = json.dumps(ordered_dict)
-        res_dict = json.loads(json_type)
-
-        itemList = res_dict['rss']['channel']['item']
-        newsList = []
-
-        for item in itemList:
-            news_dict = {
-                'title': item['title'],
-                'link': item['link'],
-                'pubDate' : item['pubDate']
-            }
-            newsList.append(news_dict)
-
-        return newsList
-    except Exception as e:
-        print(f"뉴스 파싱 중 오류 발생: {e}")
-        return []
-
-def main():
-  print(f'\n\n뉴스 기사 수집을 시작합니다...')
-  newsList = getNewsFromRss()
-  newsLen = len(newsList)
-  print(f'>>> {newsLen}개의 기사를 수집하였습니다')
-
-  print(f'\n\n뉴스 카드 생성을 시작합니다...')
-  cardList = dict()
-  cardList['attachments'] = []
-  for idx,newsItem in enumerate(newsList):
-    item = makePayloadItem(newsItem)
-    cardList['attachments'].append(item)
-    print(f'>>> [{idx}/{newsLen}] 번째 NEWS CARD를 생성하였습니다.')
-
-  print(f'\n\nSLACK 발송을 시작합니다...')
-  callWebhook(
-    {'blocks':[
-      {
-        "type": "divider"
-      },
-      {
+    제목_하이퍼링크 = f"<{newsItem['link']}|{html.unescape(newsItem['title'])}>"
+    pubDate_datetime = datetime.strptime(newsItem['pubDate'], "%a, %d %b %Y %H:%M:%S %Z")
+    pubDate_formatted = pubDate_datetime.strftime("%Y년 %m월 %d일 %H:%M")
+    return {
         "type": "section",
         "text": {
-          "type": "mrkdwn",
-          "text": "*오늘 테이블오더 뉴스*"
+            "type": "mrkdwn",
+            "text": f"{제목_하이퍼링크}\n*{pubDate_formatted}*"
         }
-      },
-      {
-        "type": "divider"
-      },
-    ]}
-  )
-  callWebhook(cardList)
+    }
+
+def callWebhook(payload):
+    headers = {
+        'Content-type': 'application/json',
+    }
+    res = requests.post(SLACK_WEBHOOK_URL, headers=headers, json=payload)
+    print(res.text)
+
+def getNewsFromRss():
+    RSS_URLS = [
+        'https://news.google.com/rss/search?hl=ko&gl=KR&ceid=KR%3Ako&oc=11&q=%22%ED%85%8C%EC%9D%B4%EB%B8%94%EC%98%A4%EB%8D%94%22%20when%3A1d',
+        'https://news.google.com/rss/search?hl=ko&gl=KR&ceid=KR%3Ako&oc=11&q=%22%ED%82%A4%EC%98%A4%EC%8A%A4%ED%81%AC%22%20when%3A1d',
+        'https://news.google.com/rss/search?hl=ko&gl=KR&ceid=KR%3Ako&oc=11&q=%22QR%EC%98%A4%EB%8D%94%22%20when%3A1d',
+        'https://news.google.com/rss/search?hl=ko&gl=KR&ceid=KR%3Ako&oc=11&q=%22QR%ED%8E%98%EC%9D%B4%22%20when%3A1d',
+        'https://news.google.com/rss/search?hl=ko&gl=KR&ceid=KR%3Ako&oc=11&q=%22KDS%22%20when%3A1d',
+    ]
+
+    allNewsList = []
+    
+    for RSS_URL in RSS_URLS:
+        try:
+            res = requests.get(RSS_URL)
+            ordered_dict = xmltodict.parse(res.text)
+            json_type = json.dumps(ordered_dict)
+            res_dict = json.loads(json_type)
+
+            itemList = res_dict['rss']['channel']['item']
+            
+            for item in itemList:
+                news_dict = {
+                    'title': item['title'],
+                    'link': item['link'],
+                    'pubDate': item['pubDate']
+                }
+                allNewsList.append(news_dict)
+        except Exception as e:
+            print(f"뉴스 파싱 중 오류 발생 ({RSS_URL}): {e}")
+
+    return allNewsList
+
+def main():
+    print(f'\n\n뉴스 기사 수집을 시작합니다...')
+    newsList = getNewsFromRss()
+    newsLen = len(newsList)
+    print(f'>>> {newsLen}개의 기사를 수집하였습니다')
+
+    print(f'\n\n뉴스 카드 생성을 시작합니다...')
+    cardList = dict()
+    cardList['blocks'] = []
+    for idx, newsItem in enumerate(newsList):
+        item = makePayloadItem(newsItem)
+        cardList['blocks'].append(item)
+        if idx < newsLen - 1:  # 마지막 기사 뒤에는 구분선을 추가하지 않음
+            cardList['blocks'].append({"type": "divider"})
+        print(f'>>> [{idx + 1}/{newsLen}] 번째 NEWS CARD를 생성하였습니다.')
+
+    print(f'\n\nSLACK 발송을 시작합니다...')
+    callWebhook(
+        {'blocks': [
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*오늘 업계 뉴스*"
+                }
+            },
+            {
+                "type": "divider"
+            },
+        ]}
+    )
+    callWebhook(cardList)
 
 if __name__ == "__main__":
-  main()
+    main()
